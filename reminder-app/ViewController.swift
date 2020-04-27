e0b23f2dbab685a9f0284479bca5b82a98d19e3c
bevcastillo <beverlymaycastillo@gmail.com>
2020-04-27 12:00:15 +0800

    Initial Commit

---
+++ reminder-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reminder-app
+//
+//  Created by Beverly May Castillo on 4/27/20.
+//  Copyright © 2020 Beverly May Castillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
